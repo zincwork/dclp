afe36905cbc80b2d68f0ff708aeca4e39824c961
Sanjeev Ponnapula <Sanjeev@Georges-MacBook-Pro.local>
2018-08-11 18:45:12 +0100

    Updates on the login flow

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,33 +1,119 @@
 <template>
- <v-layout align-center justify-center fill-height column class="bg">
-    <v-card class="pa-4 text-xs-center" @keyup.enter="submit">
-      <v-avatar class="mb-4 mt-4">
-        <v-icon color="green" size="72px">verified_user</v-icon>
-      </v-avatar>
-      <div v-if="error" class="red--text mt-5">Username and Password incorrect</div>
-      <v-text-field v-model="username" solo hide-details placeholder="Username" class="mb-3 mt-2"/>
-      <v-text-field v-model="password" solo hide-details placeholder="Password" type="password" class="mb-3"/>
-      <v-btn color="green" class="white--text" @click="submit">Submit</v-btn>
+ <v-layout align-center justify-center fill-height column>
+    <v-card class="pa-4 text-xs-center main">
+      <v-layout v-if="!loggedIn" column justify-space-around align-center fill-height>
+        <v-avatar class="mb-4 mt-4">
+          <v-icon color="green" :size="accountCreated?'128px':'72px'">verified_user</v-icon>
+        </v-avatar>
+        <h3 v-if="!accountCreated" class="mb-3">Please create your password and store them in a secure place</h3>
+        <div class="login-new" v-if="accountCreated && !ensNameSaved">
+          <v-spacer/>
+          <v-text-field v-model="ensName" solo hide-details placeholder="ENS name" suffix=".dclp.eth" class="mb-3"/>
+          <v-text-field v-model="password" solo hide-details placeholder="Password" type="password" class="mb-3"/>
+          <h3 v-if="error" class="red--text">Incorrect Password</h3>
+          <v-spacer v-else/>
+          <v-btn block color="green" class="white--text" @click="login">Login</v-btn>
+        </div>
+        <div class="signup" v-else-if="!accountCreated && !ensNameSaved">
+          <v-text-field v-model="ensName" solo hide-details placeholder="ENS name" suffix=".dclp.eth" class="mb-3"/>
+          <v-text-field v-model="password" solo hide-details placeholder="Password" type="password" class="mb-3"/>
+          <v-text-field v-model="confirmPassword" solo hide-details placeholder="Confirm password" type="password" class="mb-3"/>
+          <h3 v-if="error" class="red--text"> Passwords do not match</h3>
+          <v-spacer v-else/>
+          <v-btn block color="green" class="white--text" @click="createAccount">Create Account</v-btn>
+        </div>
+        <div class="login" v-else-if="accountCreated && ensNameSaved">
+          <!-- <v-spacer/> -->
+          <v-spacer/>
+          <v-text-field v-model="password" solo hide-details placeholder="Password" type="password" class="mb-3"/>
+          <h3 v-if="error" class="red--text">Incorrect Password</h3>
+          <v-spacer v-else/>
+          <v-btn block color="green" class="white--text" @click="submit">Submit</v-btn>
+        </div>
+        
+      </v-layout>
+      <v-layout v-else column justify-space-around align-center fill-height>
+        <h1> Log in SUCCESS!!</h1>
+        <v-btn bottom color="green" class="white--text" @click="logout">Log out</v-btn>
+      </v-layout>
     </v-card>
   </v-layout>
 </template>
 
 <script>
+import store from "store2"
+
 export default {
   data () {
     return {
-      username: null,
+      ensName: store('ensName')?store('ensName'):null,
+      accountCreated:store("accountCreated"),
+      confirmPassword: null,
       password: null,
       heading: null,
-      error: false
+      error: false,
+      loggedIn: store("loggedIn")
+    }
+  },
+  computed: {
+    ensNameSaved(){
+      return Boolean(store('ensName'))
+    },
+    url(){
+      this.$route
     }
   },
   methods: {
     submit () {
-      this.error = true
-      console.log('Here!!')
-      // this.heading = 'Success! You have logged in'
+      if(this.password === store("password")){
+        store("loggedIn",true)
+        this.reset()
+      } else{
+        this.error = true
+      }
+    },
+    login(){
+      if(this.password === store("password")){
+        console.log("logged in")
+        store("ensName",this.ensName)
+        store("loggedIn",true)
+        this.reset()
+      }
+      else{
+        this.error = true
+      }
+    },
+    createAccount(){
+      if(this.confirmPassword !== this.password && this.password === ""){
+        this.error = true
+      }
+      else {
+        store("accountCreated",true)
+        store("ensName", this.ensName)
+        store("password",this.password)
+        store("loggedIn",true)
+        this.reset()
+      }
+    },
+    logout(){
+      store("loggedIn",false)
+      this.reset()
+    },
+    reset() {
+      this.error = false
+      this.password = null
+      this.confirmPassword = null
+      this.accountCreated=store("accountCreated")
+      this.loggedIn= store("loggedIn")
+      this.ensName = store("ensName")
     }
   }
 }
 </script>
+
+<style>
+.main {
+  width: 300px !important;
+  height: 440px !important;
+}
+</style>
