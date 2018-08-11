9a6171828175721d83f52554bb76be5f048d52eb
Sanjeev Ponnapula <Sanjeev@Georges-MacBook-Pro.local>
2018-08-11 14:25:28 +0100

    Added website vue dir

---
+++ website/README.md
@@ -0,0 +1,18 @@
+# website
+
+> A website for account creation to use the dclp extension
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ website/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-layout align-center justify-center fill-height>
+      <v-card class="pa-5 text-xs-center">
+        <v-avatar class="mb-4 mt-4">
+        <v-icon color="green" size="72px">computer</v-icon>
+      </v-avatar>
+      <h1>This is the website</h1>
+      </v-card>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
