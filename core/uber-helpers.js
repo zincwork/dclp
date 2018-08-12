43e408f2d735b382a2b6f630af3957882c82807c
Ali Azam <ali.azam@kcl.ac.uk>
2018-08-12 13:23:33 +0100

    commit everything

--- core/uber-helpers.js
+++ core/uber-helpers.js
@@ -1,3 +1,4 @@
+var ipfs = require("./ipfs") 
 
 var ipfs = {}
 ipfs.qw123 = "/vNGIManRgI0QQQv8SMTnpDwMOaBP3PUyDXpOm1dcWByV4Oud90rmlogy7bQ7cEbdRyUSmJwASR8nP2g0kbmbNVezBvjWnkE8+yIgybVQl6rskZMDVsWlybMVH2BXatI"
@@ -5,7 +6,7 @@ ipfs.qw123 = "/vNGIManRgI0QQQv8SMTnpDwMOaBP3PUyDXpOm1dcWByV4Oud90rmlogy7bQ7cEbdR
 
 exports.storeCredentials = function storeCredentials(applicationName, applicationUsername, applicationPassword, applicationUrl, userEncryptionKey, userWeb3PrivateKey, userPassword, userWeb3Address) {
     var box = window.encryptionHelpers.encrypt(`{${applicationName}, ${applicationUsername}, ${applicationPassword}, ${applicationUrl} }`, userEncryptionKey)
-    window.ipfs.addToIpfs(box).then(ipfsHash => {
+      ipfs.addToIpfs(box).then(ipfsHash => {
         window.web3Helpers.set(applicationName, userPassword, ipfsHash, userWeb3PrivateKey, userWeb3Address)
     })
     
@@ -13,8 +14,8 @@ exports.storeCredentials = function storeCredentials(applicationName, applicatio
 
 exports.getCredentials = function getCredentials(application, userEncryptionKey, userPassword) {
     return new Promise(function(resolve, reject) {
-        window.web3Helpers.get(application, userPassword).then((ipfsHash) => {
-            var box = ipfs[ipfsHash]
+        window.web3Helpers.get(application, userPassword).then(async (ipfsHash) => {
+            var box = await ipfs.getFromIpfs(hash)
             var open = window.encryptionHelpers.decrypt(box, userEncryptionKey)
             resolve(open)
           
